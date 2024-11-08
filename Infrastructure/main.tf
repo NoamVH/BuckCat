32634411440b60c6d6b2601c6f06efb700a15033
NoamVH <NoamVH@gmail.com>
2024-11-08 23:46:56 +0200

    Added statebucket, formatting, and gitignore.

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -7,42 +7,56 @@ terraform {
   }
 }
 
-
 provider "google" {
   project = var.project
   region  = var.region
   zone    = var.zone
 }
 
-
 resource "google_storage_bucket" "static" {
-  name = "buckcat"
-  location = "US"
+  name          = "buckcat"
+  location      = var.region
   storage_class = "COLDLINE"
   #force_destroy = true
   uniform_bucket_level_access = true
 }
 
-
-resource "google_compute_network" "vpc_network" {
-  name = "buckcat-network"
+resource "google_compute_network" "buckcat_frontend_network" {
+  name = "buckcat-frontend-network"
 }
 
+resource "google_compute_network" "buckcat_backend_network" {
+  name = "buckcat-backend-network"
+}
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "e2-micro"
-  #tags         = ["web", "dev"]
+resource "google_cloud_run_service" "buckcat_backend_service" {
+  name = "buckcat-backend-service"
+  location = var.region
 
-  boot_disk {
-    initialize_params {
-      image = "cos-cloud/cos-stable"
-    }
-  }
-
-  network_interface {
-    network = google_compute_network.vpc_network.name
-    access_config {
+  template {
+    spec {
+      containers {
+        image = "nginx:latest"
+      }
     }
   }
 }
+
+
+# resource "google_compute_instance" "vm_instance" {
+#   name         = "buckcat-instance"
+#   machine_type = "e2-micro"
+#   #tags         = ["web", "dev"]
+
+#   boot_disk {
+#     initialize_params {
+#       image = "cos-cloud/cos-stable"
+#     }
+#   }
+
+#   network_interface {
+#     network = google_compute_network.vpc_network.name
+#     access_config {
+#     }
+#   }
+# }
