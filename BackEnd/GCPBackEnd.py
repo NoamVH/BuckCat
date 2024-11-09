7a6d63026343f22097cd4aebd44f08afa0ab92fc
NoamVH <NoamVH@gmail.com>
2024-11-09 16:51:52 +0200

    Created basic Backend program

--- BackEnd/GCPBackEnd.py
+++ BackEnd/GCPBackEnd.py
@@ -1,13 +1,30 @@
+import os
 from google.cloud import storage
 
-storage_client = storage.Client()
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '../Infrastructure/prefab-lamp-440513-v5-61362c8c30d4.json'
+
 
 BUCKCAT_NAME = "buckcat"
 
-buckcat = storage_client.bucket(BUCKCAT_NAME)
 
-print(buckcat.list_blobs())
+def create_buckcat_client():
+    storage_client = storage.Client()
+    return storage_client.bucket(BUCKCAT_NAME)
 
-# cat = buckcat.blob(source_blob_name)
+def get_cats_list(buckcat):
+    cats_iterator = buckcat.list_blobs()
+    
+    cats_list = []
+    
+    for cat_object in cats_iterator:
+        cats_list.append(cat_object.name)
+    
+    return cats_list
 
-# cat.download_to_filename("cat.png")
+def main():
+    buckcat = create_buckcat_client()
+    print(get_cats_list(buckcat))
+
+if __name__ == "__main__":
+    main()
