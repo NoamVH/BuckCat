785d6ee9bdccde8ce240eebb05e83f43bf3b9b4a
NoamVH <NoamVH@gmail.com>
2024-11-22 01:06:26 +0200

    Commented debug run for frontend

--- Frontend/GCPFrontend.py
+++ Frontend/GCPFrontend.py
@@ -105,8 +105,8 @@ def main():
     
     # The debug argument allows continous running of the webapp when changing something in the files and saving, the app will be refreshed automatically.
     # The port argument is optional, the default value is 5000.
-    app.run(debug = True, host = '0.0.0.0')
-    # app.run(debug = False, host = '0.0.0.0', port = 80)
+    # app.run(debug = True, host = '0.0.0.0')
+    app.run(debug = False, host = '0.0.0.0', port = 80)
 
 if __name__  == '__main__':
     main()
