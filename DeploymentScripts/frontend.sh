807b48d689b32891c85e7bebc17bbc40f2000db8
NoamVH <NoamVH@gmail.com>
2024-11-25 02:00:56 +0200

    Added backend CD pipeline

--- DeploymentScripts/frontend.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker stop $(docker ps -q)
-docker container prune -f
-docker image prune -a -f
-docker run -d -p 80:80 us-east1-docker.pkg.dev/$PROJECT_ID/buckcat-registry/buckcat-frontend:latest
